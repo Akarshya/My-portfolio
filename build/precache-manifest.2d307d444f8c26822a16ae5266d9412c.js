38684e692b8b89bf67d4344124b077033bba416e
Gangavarapu Akarshya <gangavarapuakarshya@gmail.com>
2024-04-19 00:04:45 +0530

    first commit

---
+++ build/precache-manifest.2d307d444f8c26822a16ae5266d9412c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "808c400c0b3478fa9374",
+    "url": "/static/css/main.95707b60.chunk.css"
+  },
+  {
+    "revision": "808c400c0b3478fa9374",
+    "url": "/static/js/main.808c400c.chunk.js"
+  },
+  {
+    "revision": "2870dd7982876d03c6f9",
+    "url": "/static/js/1.2870dd79.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afdd0d5f21bdb9a787b929bae079615e",
+    "url": "/index.html"
+  }
+];
